533229a1222b8440eb3874ad4cfb288d8ad1660c
Simon Goller <neosam@posteo.de>
2016-06-18 10:27:50 +0200

    Compile git version into binary

--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@
 
 int main(int argc, char *argv[])
 {
+    qDebug() << "Running version " << GIT_VERSION;
     QApplication a(argc, argv);
     DataIO dataIO;
     MainWindow w;
+    w.setWindowTitle(QString("Lehrerapp - Version: ") + QString(GIT_VERSION));
     w.show();
 
     return a.exec();
